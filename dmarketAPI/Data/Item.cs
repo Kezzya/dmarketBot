8dc6a8231b18fa0a7cfcb8e8fd39c098ceafd0a9
Nikita <kobzevnikita0@mail.ru>
2020-04-28 15:21:45 +0300

    Adding files

---
+++ dmarketAPI/Data/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmarketAPI.Data
+{
+	public class DmarketItem
+	{
+		public Item[] objects { get; set; }
+	}
+	public class Item
+	{
+		public int discount { get; set; }
+		public string title { get; set; }
+	}
+}
---
+++ dmarketApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using dmarketAPI.Data;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading;
+using System.Web;
+
+namespace dmarketApp
+{
+	class Program
+	{
+
+		static readonly HttpClient client = new HttpClient();
+		static async Task Main(string[] args)
+		{
+
+			while (true)
+			{
+				try
+				{
+					string link =
+				   "https://api.dmarket.com/exchange/v1/market/items?orderBy=best_deals&orderDir=desc&title=&priceFrom=0&priceTo=4370&treeFilters=&gameId=a8db&offset=0&limit=100&currency=USD";
+					HttpResponseMessage response = await client.GetAsync(link);
+					response.EnsureSuccessStatusCode();
+					string responseBody = await response.Content.ReadAsStringAsync();
+
+					var firstItem = JsonConvert.DeserializeObject<DmarketItem>(responseBody);
+				
+					foreach (var item in firstItem.objects)
+					{
+
+						if (item.discount >= 23 && item.title != "CS:GO Weapon Case")
+						{
+							System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"music/дора.wav");
+							player.Play();
+							Console.WriteLine(item.discount);
+							Console.WriteLine(item.title);
+							Console.ReadLine();
+							player.Stop();
+						}
+					}
+				}
+				catch (HttpRequestException e)
+				{
+					Console.WriteLine("\nException Caught!");
+					Console.WriteLine("Message :{0} ", e.Message);
+				}
+				Thread.Sleep(200);
+			}
+
+		}
+	}
+
+}
